18977c60f1ffa3f47c37ddd7923b5bb18976284f
Florian <florian.pepin@univ-rouen.fr>
2026-03-30 14:57:10 +0200

    fix error controller spring boot 4

--- src/main/java/fr/univrouen/sepa26/controllers/CustomErrorController.java
+++ src/main/java/fr/univrouen/sepa26/controllers/CustomErrorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class CustomErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+public class CustomErrorController {
 
     @RequestMapping("/error")
     @ResponseBody
