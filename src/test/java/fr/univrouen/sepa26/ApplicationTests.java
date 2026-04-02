263431cd6568e924d7090cc71927199d7e2f36f1
Florian <florian.pepin@univ-rouen.fr>
2026-04-02 15:21:54 +0200

    fix: base de données h2 tests

--- src/test/java/fr/univrouen/sepa26/ApplicationTests.java
+++ src/test/java/fr/univrouen/sepa26/ApplicationTests.java
@@ -2,8 +2,10 @@ package fr.univrouen.sepa26;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
 
 	@Test
