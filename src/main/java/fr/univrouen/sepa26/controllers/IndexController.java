bb0437a613ff3cc377f4108428317a4f47150ca0
ouattara <ummhabibah-ouattara@outlook.com>
2026-04-03 13:36:23 +0200

    help

--- src/main/java/fr/univrouen/sepa26/controllers/IndexController.java
+++ src/main/java/fr/univrouen/sepa26/controllers/IndexController.java
@@ -1,12 +1,17 @@
 package fr.univrouen.sepa26.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 	@GetMapping("/")
-	public String index() {
-		return "Hello sepa26 !";
+	public String home(Model model) {
+		model.addAttribute("projectName", "Projet XML Sepa26");
+		model.addAttribute("version", "1.0");
+		model.addAttribute("developers", "Florian & Umm-Habibah");
+		model.addAttribute("universityLogo", "https://www.choisirlanormandie.fr/app/uploads/2024/08/logo-universite-de-rouen.png");
+		return "landing"; // le nom du fichier thymeleaf
 	}
 }
